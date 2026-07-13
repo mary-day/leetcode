fdfa379e0e521ffa8615bb08a30a5370f5fd2cdf
Mary Day <91143090+mary-day@users.noreply.github.com>
2026-07-13 10:55:22 -0700

    Time: 15 ms (32.65%) | Memory: 62.9 MB (59.59%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    const stack = [];
+
+    for (const char of s) {
+        if (stack.length && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.join("");
+};
