3b01d5425d1b8089189ffc928b31ec145e032590
Mary Day <91143090+mary-day@users.noreply.github.com>
2026-07-13 09:54:48 -0700

    Time: 0 ms (100.00%) | Memory: 53.9 MB (51.92%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -6,9 +6,9 @@
 var searchInsert = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
-
+    
     while (left <= right) {
-        const mid = Math.floor((left + right)/2);
+        let mid = Math.floor((left + right)/2);
         if (nums[mid] === target) {
             return mid;
         } else if (nums[mid] < target) {
@@ -17,6 +17,5 @@ var searchInsert = function(nums, target) {
             right = mid - 1;
         }
     }
-
     return left;
 };
